e362eff99486675f454162f14120f86279801888
Vansh1379 <vanshkalra1379@gmail.com>
2025-11-05 23:35:13 +0530

    bbb

--- includes/header.php
+++ includes/header.php
@@ -60,10 +60,32 @@ $show_header_top = shouldShowHeaderTop();
             <?php
             global $navigation_menu;
             foreach ($navigation_menu as $page => $title):
+                // Check if this is the Products menu item
+                if ($page == 'products'):
+                    $active_class = (in_array($current_page, ['products', 'playground-equipment', 'outdoor-gym', 'indoor-gym'])) ? 'class="active"' : '';
+            ?>
+            <li class="nav-dropdown">
+                <a href="products.php" <?php echo $active_class; ?>>
+                    <?php echo $title; ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" class="dropdown-icon">
+                        <polyline points="6 9 12 15 18 9"></polyline>
+                    </svg>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="products.php">All Products</a></li>
+                    <li><a href="playground-equipment.php">Playground Equipment</a></li>
+                    <li><a href="outdoor-gym.php">Outdoor Gym</a></li>
+                    <li><a href="indoor-gym.php">Indoor Gym</a></li>
+                </ul>
+            </li>
+            <?php else:
                 $active_class = ($current_page == $page) ? 'class="active"' : '';
             ?>
             <li><a href="<?php echo $page; ?>.php" <?php echo $active_class; ?>><?php echo $title; ?></a></li>
-            <?php endforeach; ?>
+            <?php 
+                endif;
+            endforeach; 
+            ?>
         </ul>
     </nav>
 </header>
